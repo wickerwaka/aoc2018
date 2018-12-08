8d4b6f6c288fdd525ec889bcf979b4d283ace777
Martin Donlon <github-martin@donlons.com>
2018-12-07 22:11:23 -0800

    Cleanup
    Switch to using an iterator instead of a reversed vec
    Better Result<> usage

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -10,61 +10,58 @@ struct Node {
 }
 
 impl Node {
-    fn from_vec(v: &mut Vec<i32>) -> Node {
-        let child_count = v.pop().unwrap();
-        let meta_count = v.pop().unwrap();
+    fn from_iter<I>(v: &mut I) -> Result<Node, &'static str>
+    where
+        I: Iterator<Item = i32>,
+    {
+        let child_count = v.next().ok_or("missing child")?;
+        let meta_count = v.next().ok_or("missing meta")?;
 
         let mut children = Vec::new();
         let mut metasum = 0;
-        let mut value = 0;
 
         for _ in 0..child_count {
-            let child = Node::from_vec(v);
+            let child = Node::from_iter(v)?;
             metasum += child.metasum;
             children.push(child);
         }
 
         let mut meta = Vec::new();
         for _ in 0..meta_count {
-            let m = v.pop().unwrap();
+            let m = v.next().ok_or("short meta read")?;
             metasum += m;
             meta.push(m);
         }
 
-        if child_count == 0 {
-            value = meta.iter().sum();
-        } else {
-            value = meta
+        let value = match child_count {
+            0 => meta.iter().sum(),
+            _ => meta
                 .iter()
                 .filter_map(|x| {
-                    if x == &0 {
+                    if *x == 0 {
                         None
                     } else if let Some(child) = children.get((x - 1) as usize) {
                         Some(child.value)
                     } else {
                         None
                     }
-                }).sum();
-        }
+                }).sum(),
+        };
 
-        Node {
+        Ok(Node {
             metadata: meta,
             children: children,
             metasum: metasum,
             value: value,
-        }
+        })
     }
 }
 
 fn main() -> io::Result<()> {
     let s = fs::read_to_string("input/input.txt")?;
 
-    let mut series: Vec<i32> = s
-        .split_whitespace()
-        .filter_map(|x| x.parse::<i32>().ok())
-        .collect();
-    series.reverse();
-    let root = Node::from_vec(&mut series);
+    let mut series = s.split_whitespace().filter_map(|x| x.parse::<i32>().ok());
+    let root = Node::from_iter(&mut series).unwrap();
     println!("Part1: {}", root.metasum);
     println!("Part2: {}", root.value);
 
